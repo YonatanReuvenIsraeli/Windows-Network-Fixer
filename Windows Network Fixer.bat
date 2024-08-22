050c30b2ef005b6562877ad5d78b4c17e92fe296
Yonatan Israeli <yonirisraeli@gmail.com>
2024-08-21 20:45:55 -0400

    Update Windows Network Fixer.bat

--- Windows Network Fixer.bat
+++ Windows Network Fixer.bat
@@ -2,7 +2,7 @@
 title Windows Network Fixer
 setlocal
 echo Program Name: Windows Network Fixer
-echo Version: 1.0.6
+echo Version: 1.0.7
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -97,7 +97,7 @@ goto "Restart"
 :"reginiExist"
 set regini=True
 echo.
-echo Please temporary rename to something else or temporary move to another location "%cd%\regini.txt" in order for this batch file to proceed. Press any key to continue when "%cd%\regini.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.
+echo Please temporary rename to something else or temporary move to another location "%cd%\regini.txt" in order for this batch file to proceed. "%cd%\regini.txt" is not a system file. Press any key to continue when "%cd%\regini.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.
 pause > nul 2>&1
 goto "5"
 
